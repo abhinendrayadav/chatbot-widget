951687a9d6cb2df58adbe63f385d41ad5d3020b0
Abhinendra Yadav <AYadav@ad.prolifics.com>
2020-02-24 13:29:25 +0530

    removed charts

--- static/js/script.js
+++ static/js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 	$('.dropdown-trigger').dropdown();
 
 	//initiate the modal for displaying the charts, if you dont have charts, then you comment the below line
-	$('.modal').modal();
+	// $('.modal').modal();
 
 	//enable this if u have configured the bot to start the conversation. 
 	// showBotTyping();
@@ -24,12 +24,12 @@ $(document).ready(function () {
 function restartConversation() {
 	$("#userInput").prop('disabled', true);
 	//destroy the existing chart
-	$('.collapsible').remove();
+	// $('.collapsible').remove();
 
-	if (typeof chatChart !== 'undefined') { chatChart.destroy(); }
+	// if (typeof chatChart !== 'undefined') { chatChart.destroy(); }
 
-	$(".chart-container").remove();
-	if (typeof modalChart !== 'undefined') { modalChart.destroy(); }
+	// $(".chart-container").remove();
+	// if (typeof modalChart !== 'undefined') { modalChart.destroy(); }
 	$(".chats").html("");
 	$(".usrInput").val("");
 	send("/restart");
@@ -74,11 +74,11 @@ $(".usrInput").on("keyup keypress", function (e) {
 			return false;
 		} else {
 			//destroy the existing chart, if yu are not using charts, then comment the below lines
-			$('.collapsible').remove();
-			if (typeof chatChart !== 'undefined') { chatChart.destroy(); }
+			// $('.collapsible').remove();
+			// if (typeof chatChart !== 'undefined') { chatChart.destroy(); }
 
-			$(".chart-container").remove();
-			if (typeof modalChart !== 'undefined') { modalChart.destroy(); }
+			// $(".chart-container").remove();
+			// if (typeof modalChart !== 'undefined') { modalChart.destroy(); }
 
 
 
@@ -103,14 +103,14 @@ $("#sendButton").on("click", function (e) {
 	else {
 		//destroy the existing chart
 
-		chatChart.destroy();
-		$(".chart-container").remove();
-		if (typeof modalChart !== 'undefined') { modalChart.destroy(); }
+		// chatChart.destroy();
+		// $(".chart-container").remove();
+		// if (typeof modalChart !== 'undefined') { modalChart.destroy(); }
 
-		$(".suggestions").remove();
-		$("#paginated_cards").remove();
-		$(".quickReplies").remove();
-		$(".usrInput").blur();
+		// $(".suggestions").remove();
+		// $("#paginated_cards").remove();
+		// $(".quickReplies").remove();
+		// $(".usrInput").blur();
 		setUserResponse(text);
 		send(text);
 		e.preventDefault();
@@ -278,34 +278,6 @@ $("#profile_div").click(function () {
 	$(".widget").toggle();
 });
 
-//====================================== Suggestions ===========================================
-
-function addSuggestion(textToAdd) {
-	setTimeout(function () {
-		var suggestions = textToAdd;
-		var suggLength = textToAdd.length;
-		$(' <div class="singleCard"> <div class="suggestions"><div class="menu"></div></div></diV>').appendTo(".chats").hide().fadeIn(1000);
-		// Loop through suggestions
-		for (i = 0; i < suggLength; i++) {
-			$('<div class="menuChips" data-payload=\'' + (suggestions[i].payload) + '\'>' + suggestions[i].title + "</div>").appendTo(".menu");
-		}
-		scrollToBottomOfResults();
-	}, 1000);
-}
-
-// on click of suggestions, get the value and send to rasa
-$(document).on("click", ".menu .menuChips", function () {
-	var text = this.innerText;
-	var payload = this.getAttribute('data-payload');
-	console.log("payload: ", this.getAttribute('data-payload'))
-	setUserResponse(text);
-	send(payload);
-
-	//delete the suggestions once user click on it
-	$(".suggestions").remove();
-
-});
-
 //====================================== functions for drop-down menu of the bot  =========================================
 
 //restart function to restart the conversation.
@@ -328,164 +300,6 @@ $("#close").click(function () {
 	scrollToBottomOfResults();
 });
 
-//====================================== Cards Carousel =========================================
-
-function showCardsCarousel(cardsToAdd) {
-	var cards = createCardsCarousel(cardsToAdd);
-
-	$(cards).appendTo(".chats").show();
-
-
-	if (cardsToAdd.length <= 2) {
-		$(".cards_scroller>div.carousel_cards:nth-of-type(" + i + ")").fadeIn(3000);
-	}
-	else {
-		for (var i = 0; i < cardsToAdd.length; i++) {
-			$(".cards_scroller>div.carousel_cards:nth-of-type(" + i + ")").fadeIn(3000);
-		}
-		$(".cards .arrow.prev").fadeIn("3000");
-		$(".cards .arrow.next").fadeIn("3000");
-	}
-
-
-	scrollToBottomOfResults();
-
-	const card = document.querySelector("#paginated_cards");
-	const card_scroller = card.querySelector(".cards_scroller");
-	var card_item_size = 225;
-
-	card.querySelector(".arrow.next").addEventListener("click", scrollToNextPage);
-	card.querySelector(".arrow.prev").addEventListener("click", scrollToPrevPage);
-
-
-	// For paginated scrolling, simply scroll the card one item in the given
-	// direction and let css scroll snaping handle the specific alignment.
-	function scrollToNextPage() {
-		card_scroller.scrollBy(card_item_size, 0);
-	}
-	function scrollToPrevPage() {
-		card_scroller.scrollBy(-card_item_size, 0);
-	}
-
-}
-
-function createCardsCarousel(cardsData) {
-
-	var cards = "";
-
-	for (i = 0; i < cardsData.length; i++) {
-		title = cardsData[i].name;
-		ratings = Math.round((cardsData[i].ratings / 5) * 100) + "%";
-		data = cardsData[i];
-		item = '<div class="carousel_cards in-left">' + '<img class="cardBackgroundImage" src="' + cardsData[i].image + '"><div class="cardFooter">' + '<span class="cardTitle" title="' + title + '">' + title + "</span> " + '<div class="cardDescription">' + '<div class="stars-outer">' + '<div class="stars-inner" style="width:' + ratings + '" ></div>' + "</div>" + "</div>" + "</div>" + "</div>";
-
-		cards += item;
-	}
-
-	var cardContents = '<div id="paginated_cards" class="cards"> <div class="cards_scroller">' + cards + '  <span class="arrow prev fa fa-chevron-circle-left "></span> <span class="arrow next fa fa-chevron-circle-right" ></span> </div> </div>';
-
-	return cardContents;
-}
-
-//====================================== Quick Replies ==================================================
-
-function showQuickReplies(quickRepliesData) {
-	var chips = ""
-	for (i = 0; i < quickRepliesData.length; i++) {
-		var chip = '<div class="chip" data-payload=\'' + (quickRepliesData[i].payload) + '\'>' + quickRepliesData[i].title + '</div>'
-		chips += (chip)
-	}
-
-	var quickReplies = '<div class="quickReplies">' + chips + '</div><div class="clearfix"></div>'
-	$(quickReplies).appendTo(".chats").fadeIn(1000);
-	scrollToBottomOfResults();
-	const slider = document.querySelector('.quickReplies');
-	let isDown = false;
-	let startX;
-	let scrollLeft;
-
-	slider.addEventListener('mousedown', (e) => {
-		isDown = true;
-		slider.classList.add('active');
-		startX = e.pageX - slider.offsetLeft;
-		scrollLeft = slider.scrollLeft;
-	});
-	slider.addEventListener('mouseleave', () => {
-		isDown = false;
-		slider.classList.remove('active');
-	});
-	slider.addEventListener('mouseup', () => {
-		isDown = false;
-		slider.classList.remove('active');
-	});
-	slider.addEventListener('mousemove', (e) => {
-		if (!isDown) return;
-		e.preventDefault();
-		const x = e.pageX - slider.offsetLeft;
-		const walk = (x - startX) * 3; //scroll-fast
-		slider.scrollLeft = scrollLeft - walk;
-	});
-
-}
-
-// on click of quickreplies, get the value and send to rasa
-$(document).on("click", ".quickReplies .chip", function () {
-	var text = this.innerText;
-	var payload = this.getAttribute('data-payload');
-	console.log("chip payload: ", this.getAttribute('data-payload'))
-	setUserResponse(text);
-	send(payload);
-
-	//delete the quickreplies
-	$(".quickReplies").remove();
-
-});
-
-//====================================== Get User Location ==================================================
-function getLocation() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(getUserPosition, handleLocationAccessError);
-	} else {
-		response = "Geolocation is not supported by this browser.";
-	}
-}
-
-function getUserPosition(position) {
-	response = "Latitude: " + position.coords.latitude + " Longitude: " + position.coords.longitude;
-	console.log("location: ", response);
-
-	//here you add the intent which you want to trigger 
-	response = '/inform{"latitude":' + position.coords.latitude + ',"longitude":' + position.coords.longitude + '}';
-	$("#userInput").prop('disabled', false);
-	send(response);
-	showBotTyping();
-}
-
-function handleLocationAccessError(error) {
-
-	switch (error.code) {
-		case error.PERMISSION_DENIED:
-			console.log("User denied the request for Geolocation.")
-			break;
-		case error.POSITION_UNAVAILABLE:
-			console.log("Location information is unavailable.")
-			break;
-		case error.TIMEOUT:
-			console.log("The request to get user location timed out.")
-			break;
-		case error.UNKNOWN_ERROR:
-			console.log("An unknown error occurred.")
-			break;
-	}
-
-	response = '/inform{"user_location":"deny"}';
-	send(response);
-	showBotTyping();
-	$(".usrInput").val("");
-	$("#userInput").prop('disabled', false);
-
-
-}
 
 //======================================bot typing animation ======================================
 function showBotTyping() {
@@ -525,112 +339,5 @@ function createCollapsible(data) {
 }
 
 
-//====================================== creating Charts ======================================
 
-//function to create the charts & render it to the canvas
-function createChart(title, labels, backgroundColor, chartsData, chartType, displayLegend) {
-
-	//create the ".chart-container" div that will render the charts in canvas as required by charts.js,
-	// for more info. refer: https://www.chartjs.org/docs/latest/getting-started/usage.html
-	var html = '<div class="chart-container"> <span class="modal-trigger" id="expand" title="expand" href="#modal1"><i class="fa fa-external-link" aria-hidden="true"></i></span> <canvas id="chat-chart" ></canvas> </div> <div class="clearfix"></div>'
-	$(html).appendTo('.chats');
-
-	//create the context that will draw the charts over the canvas in the ".chart-container" div
-	var ctx = $('#chat-chart');
-
-	// Once you have the element or context, instantiate the chart-type by passing the configuration,
-	//for more info. refer: https://www.chartjs.org/docs/latest/configuration/
-	var data = {
-		labels: labels,
-		datasets: [{
-			label: title,
-			backgroundColor: backgroundColor,
-			data: chartsData,
-			fill: false
-		}]
-	};
-	var options = {
-		title: {
-			display: true,
-			text: title
-		},
-		layout: {
-			padding: {
-				left: 5,
-				right: 0,
-				top: 0,
-				bottom: 0
-			}
-		},
-		legend: {
-			display: displayLegend,
-			position: "right",
-			labels: {
-				boxWidth: 5,
-				fontSize: 10
-			}
-		}
-	}
-
-	//draw the chart by passing the configuration
-	chatChart = new Chart(ctx, {
-		type: chartType,
-		data: data,
-		options: options
-	});
-
-	scrollToBottomOfResults();
-}
-
-// on click of expand button, get the chart data from gloabl variable & render it to modal
-$(document).on("click", "#expand", function () {
-
-	//the parameters are declared gloabally while we get the charts data from rasa.
-	createChartinModal(title, labels, backgroundColor, chartsData, chartType, displayLegend)
-});
-
-//function to render the charts in the modal
-function createChartinModal(title, labels, backgroundColor, chartsData, chartType, displayLegend) {
-	//if you want to display the charts in modal, make sure you have configured the modal in index.html
-	//create the context that will draw the charts over the canvas in the "#modal-chart" div of the modal
-	var ctx = $('#modal-chart');
-
-	// Once you have the element or context, instantiate the chart-type by passing the configuration,
-	//for more info. refer: https://www.chartjs.org/docs/latest/configuration/
-	var data = {
-		labels: labels,
-		datasets: [{
-			label: title,
-			backgroundColor: backgroundColor,
-			data: chartsData,
-			fill: false
-		}]
-	};
-	var options = {
-		title: {
-			display: true,
-			text: title
-		},
-		layout: {
-			padding: {
-				left: 5,
-				right: 0,
-				top: 0,
-				bottom: 0
-			}
-		},
-		legend: {
-			display: displayLegend,
-			position: "right"
-		},
-
-	}
-
-	modalChart = new Chart(ctx, {
-		type: chartType,
-		data: data,
-		options: options
-	});
-
-}
 
